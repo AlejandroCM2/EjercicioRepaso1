0fa2b27e11d38a1647e6db02d9f5f864b211fe79
AlejandroCM <alejandromonsalve2013@gmail.com>
2023-08-01 21:09:25 -0500

    Escribir un programa que determine si un número dado es par o impar.

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -1,10 +1,10 @@
 def es_par(numero):
     return numero % 2 == 0
 
-if __name__ == "__main__":
-    numero = int(input("Ingrese un número: "))
+numero = int(input("Ingrese un número: "))
 
-    if es_par(numero):
-        print(f"{numero} es un número par.")
-    else:
-        print(f"{numero} es un número impar.")
+if es_par(numero):
+    print(f"{numero} es un número par.")
+else:
+    print(f"{numero} es un número impar.")
+
