fb9969ff70c02f647a3a694d2e3a3243a083264d
AlejandroCM <alejandromonsalve2013@gmail.com>
2023-08-01 21:18:21 -0500

    Crear una función que invierta el orden de los elementos en una lista dada.

---
+++ Ejercicio_8.py
@@ -0,0 +1,8 @@
+def invertir_lista(lista):
+    lista_invertida = lista[::-1]
+    return lista_invertida
+
+
+mi_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_invertida = invertir_lista(mi_lista)
+print(lista_invertida)
